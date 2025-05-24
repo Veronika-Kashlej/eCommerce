b9db920736eebfd71bdc625c059aa64204f22ccc
Veronika Kashlej <veronika.kashlei@mail.ru>
2025-05-24 20:19:09 +0300

    feat: Add some styles for page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,20 +1,13 @@
 import './Home.css';
-import { useState } from 'react';
-import api from '@/api/api';
+// import { useState } from 'react';
+// import api from '@/api/api';
 import ProductList from '@/components/products-list/Products-list';
 
 const HomePage: React.FC = () => {
-  const [isAuthenticated] = useState(api.loginned);
+  // const [isAuthenticated] = useState(api.loginned);
   return (
     <div className="page-container">
       <main className="main-content">
-        <h1 className="welcome-title">Welcome to our service</h1>
-        <p className="welcome-text">
-          {isAuthenticated
-            ? "You're successfully logged in!"
-            : 'Please sign in or register to continue'}
-        </p>
-
         <ProductList />
       </main>
     </div>
