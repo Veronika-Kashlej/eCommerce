28734aa60b1c5dc62030dd82ad68f8a4eec0a2d7
KirrBrest <lukashchuk.kiryl@yandex.by>
2025-06-11 02:09:18 +0300

    feat: Add empty basket message (Wed, Jun 11, 2025 02:09:18 AM)

--- src/pages/basket/BasketPage.tsx
+++ src/pages/basket/BasketPage.tsx
@@ -1,5 +1,29 @@
+import React, { useState } from 'react';
+import EmptyMessage from './EmptyMessage';
+import { ProductCardProps } from '@/types/interfaces';
+import ProductCard from '@/components/ProductCard/ProductCard';
+
 const BasketPage: React.FC = () => {
-  return <div className="page-container"></div>;
+  const [isCartEmpty, setIsCartEmpty] = useState(true);
+
+  const handleCartStatusChange = (empty: boolean) => {
+    setIsCartEmpty(empty);
+  };
+
+  const items: ProductCardProps[] = []; // тут надо заполнить товарами которые должны быть в корзине
+
+  return (
+    <div className="page-container">
+      <EmptyMessage onStatusChange={handleCartStatusChange} />
+      {!isCartEmpty ? ( // тут код списка товаров
+        <div className="product-list">
+          {items.map((item, index) => (
+            <ProductCard key={item.product.key ?? index} {...item} />
+          ))}
+        </div>
+      ) : null}
+    </div>
+  );
 };
 
 export default BasketPage;
---
+++ src/pages/basket/EmptyMessage.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import api from '@/api/api';
+
+const EmptyMessage: React.FC<{ onStatusChange?: (isEmpty: boolean) => void }> = ({
+  onStatusChange,
+}) => {
+  const [isEmpty, setIsEmpty] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = api.subscribeToCartChanges((cartIsEmpty: boolean) => {
+      setIsEmpty(cartIsEmpty);
+      if (onStatusChange) {
+        onStatusChange(cartIsEmpty);
+      }
+    });
+
+    api.isCartEmpty().then((isEmpty: boolean) => {
+      setIsEmpty(isEmpty);
+      if (onStatusChange) {
+        onStatusChange(isEmpty);
+      }
+    });
+
+    return () => {
+      if (unsubscribe) {
+        unsubscribe();
+      }
+    };
+  }, [onStatusChange]);
+
+  if (!isEmpty) {
+    return null;
+  }
+
+  return (
+    <div
+      style={{
+        padding: '20px',
+        textAlign: 'center',
+        backgroundColor: '#f9f9f9',
+        borderRadius: '8px',
+        fontSize: '1.2em',
+        color: '#333',
+      }}
+    >
+      🛒 Oops! Your bag is empty. Start shopping and add products! 🚀
+    </div>
+  );
+};
+
+export default EmptyMessage;
